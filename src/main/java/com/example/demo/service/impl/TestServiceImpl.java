5183fb15c080c587b135bcb619d9f0b8fd89b1bb
smartkk <752590950@qq.com>
2019-12-11 17:34:32 +0800

    .

--- src/main/java/com/example/demo/service/impl/TestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TestServiceImpl.java
@@ -12,6 +12,6 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public String sayHello() {
-        return "TestService say: "+name;
+        return "TestService say hello to: "+name;
     }
 }
